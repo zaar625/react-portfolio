2fa59778474a265907a8b89d95755add4ba3c72b
zaar625 <zaar625@naver.com>
2022-06-03 01:50:39 +0900

    firebase 오류로 인해 홈페이지 빌드가 되지 않아 수정

--- src/firebase/firebaseInit.js
+++ src/firebase/firebaseInit.js
@@ -1,10 +1,5 @@
 import firebase from 'firebase/app';
 // import 'firebase/auth';
-import 'firebase/firestore';
-import 'firebase/storage';
-import 'firebase/auth';
-import 'firebase/database';
-
 
 const firebaseConfig = {
     apiKey: process.env.REACT_APP_FIREBASE_KEY,
