ec237639d9ef1e576da792b20a4f662db65f0f59
zaar625 <zaar625@naver.com>
2022-06-21 20:48:37 +0900

    webp경로 오류

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,33 +1,33 @@
 export const skillImages01 = [
-    require('./images/skill/html.jpg'),
-    require('./images/skill/css.jpg'),
-    require('./images/skill/js.jpg'),
-    require('./images/skill/react.jpg'),
-    require('./images/skill/sass.jpg'),
-    require('./images/skill/styled.jpg'),
-    require('./images/skill/typescript.jpg'),
-    require('./images/skill/vue.jpg'),
+    require('./images/skill/html.webp'),
+    require('./images/skill/css.webp'),
+    require('./images/skill/js.webp'),
+    require('./images/skill/react.webp'),
+    require('./images/skill/sass.webp'),
+    require('./images/skill/styled.webp'),
+    require('./images/skill/typescript.webp'),
+    require('./images/skill/vue.webp'),
 ]
 
 export const skillImages02 = [
-    require('./images/skill/ai.jpg'),
-    require('./images/skill/ps.jpg'),
-    require('./images/skill/figma.jpg'),
-    require('./images/skill/firebase.jpg'),
-    require('./images/skill/github.jpg'),
-    require('./images/skill/notion.jpg'),
+    require('./images/skill/ai.webp'),
+    require('./images/skill/ps.webp'),
+    require('./images/skill/figma.webp'),
+    require('./images/skill/firebase.webp'),
+    require('./images/skill/github.webp'),
+    require('./images/skill/notion.webp'),
 ]
 
 export const experience = [
-    require('./images/social/coffeenie.jpg'),
-    require('./images/social/starbucks.jpg'),
-    require('./images/social/mein.jpg'),
-    require('./images/social/zara.jpg'),
+    require('./images/social/coffeenie.webp'),
+    require('./images/social/starbucks.webp'),
+    require('./images/social/mein.webp'),
+    require('./images/social/zara.webp'),
 ]
 
 export const projectImages = [
     require('./images/project/bbyb.jpg'),
-    require('./images/project/movie.jpg'),
+    require('./images/project/movie.jpggit '),
     require('./images/project/interactive.jpg'),
     require('./images/project/plant.jpg'),
     require('./images/project/basic.jpg'),
